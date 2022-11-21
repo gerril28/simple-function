520ccaf0036ad306b2c1d79ca0ad777969a4f7e5
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-21 09:19:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+function counter () {
+    for (let index = 0; index <= 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(3 - index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("GO !")
+}
+input.onButtonPressed(Button.A, function () {
+    counter()
+})
+basic.forever(function () {
+	
+})
